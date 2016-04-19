174b55c702be63b12f4be3867bacb45185cb9e88
Aaron <Aaron@DESKTOP-BCLEH27>
2016-04-19 09:36:48 +0200

    final

---
+++ src/boletin_25_3/Boletin_25_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_25_3;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Boletin_25_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana vvv = new Ventana();
+        vvv.setVisible(true);
+    }
+    
+}
